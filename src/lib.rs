dd623efd8da6d4b85f4abaa5fe4e466e4af7c01d
Hamzah Ahmed <hamzah@syedahmed.net>
2023-01-01 21:58:47 +1100

    feat: add sphere with normal texture

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,28 @@ use crate::math::{Point3, Vector3};
 
 pub mod math;
 
+fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> f64 {
+    let oc = ray.origin - center;
+    let a = Vector3::dot(&ray.direction, &ray.direction);
+    let b = 2.0 * Vector3::dot(&oc, &ray.direction);
+    let c = Vector3::dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
+}
+
 fn ray_color(ray: Ray) -> Color {
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray);
+
+    if t > 0.0 {
+        let normal = (ray.at(t) - Vector3::new(0.0, 0.0, -1.0)).unit_vector();
+        return 0.5 * Color::new(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0);
+    }
+
     let unit_direction = ray.direction.unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
 
